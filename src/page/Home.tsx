0b087f154f19ebc59be6bf6826a1b715f5684132
AiFahri <fahrinurilll@gmail.com>
2025-06-23 22:56:04 +0700

    initial commit

---
+++ src/page/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold mb-4">Welcome to My App</h1>
+      <p className="text-lg text-red-700">
+        This is a simple React application.
+      </p>
+    </div>
+  );
+};
+export default Home;
