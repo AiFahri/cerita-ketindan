3e69e3f327668fb093109324028ff93eaefa00d4
AiFahri <fahrinurilll@gmail.com>
2025-06-26 23:10:48 +0700

    feat: enable Home page route in application routing

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import CountdownPage from "../pages/CountdownPage";
-// import Home from "../pages/Home";
+import Home from "../pages/Home";
 // import DetailPage from "../pages/DetailPage";
 import NotFound from "../pages/NotFound";
 
@@ -9,13 +9,9 @@ const AppRoutes = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<CountdownPage />} />
-
-        {/* <Route path="/home" element={<Home />} />
-
-        <Route path="/detail/:slug" element={<DetailPage />} /> */}
-
+        <Route path="/home" element={<Home />} />
+        {/* <Route path="/detail/:slug" element={<DetailPage />} /> */}
         <Route path="/404" element={<NotFound />} />
-
         <Route path="*" element={<Navigate to="/404" replace />} />
       </Routes>
     </BrowserRouter>
