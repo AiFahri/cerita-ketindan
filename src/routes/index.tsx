ae1e7b983a67d88df34770c008fb017355a87bad
AiFahri <fahrinurilll@gmail.com>
2025-06-27 07:46:08 +0700

    feat: update routing to include DetailPage for various sections and add Vercel configuration

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import CountdownPage from "../pages/CountdownPage";
 import Home from "../pages/Home";
-// import DetailPage from "../pages/DetailPage";
+import DetailPage from "../pages/DetailPage";
 import NotFound from "../pages/NotFound";
 import Analytics from "../components/Analytics";
 
@@ -12,7 +12,9 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<CountdownPage />} />
         <Route path="/home" element={<Home />} />
-        {/* <Route path="/detail/:slug" element={<DetailPage />} /> */}
+        <Route path="/wisata/:slug" element={<DetailPage />} />
+        <Route path="/peninggalan/:slug" element={<DetailPage />} />
+        <Route path="/sejarah/:slug" element={<DetailPage />} />
         <Route path="/404" element={<NotFound />} />
         <Route path="*" element={<Navigate to="/404" replace />} />
       </Routes>
