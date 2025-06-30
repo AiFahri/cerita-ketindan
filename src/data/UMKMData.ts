555e2ece8da3fa197bf7b4349cf7bd265997dfa8
AiFahri <fahrinurilll@gmail.com>
2025-07-01 02:51:50 +0700

    feat: update UMKMData with corrected image paths and add W-dang Waras product

--- src/data/UMKMData.ts
+++ src/data/UMKMData.ts
@@ -1,7 +1,8 @@
 import keripik from "/keripik arjuna.jpg";
-import pia from "/pia nagaa.jpg";
+import pia from "/Pia Naga.jpg";
 import kopi from "/kopi.jpg";
 import batik from "/batik tulis.png";
+import wdang from "/wedang waras.jpg";
 
 export interface UMKMData {
   slug: string;
@@ -34,9 +35,7 @@ export const UMKMData: UMKMData[] = [
       "Keripik hasil olahan pertanian lokal Desa Ketindan, dirintis oleh Ibu Tarwiyah dengan rasa gurih dan renyah yang digemari banyak orang.",
     detailDescription:
       "Keripik Arjuna adalah salah satu produk UMKM unggulan Desa Ketindan yang telah dirintis oleh Ibu Tarwiyah sejak 12 tahun lalu, bermula dari modal Rp250.000. Awalnya hanya memproduksi keripik singkong, usaha ini kemudian berkembang dengan menambah varian seperti keripik pisang, nangka, dan talas. Bu Tarwiyah secara khusus menggunakan bahan baku dari Desa Ketindan sendiri, karena meyakini kontur tanah yang subur di desa menghasilkan cita rasa keripik yang lebih enak dan autentik. Produk Keripik Arjuna sangat populer dan distribusinya telah meluas hingga ke luar Desa Ketindan dan Kota Lawang melalui berbagai distributor. Usaha yang awalnya dirintis berdua dengan suami ini, kini telah mempekerjakan 4 orang karyawan, dengan kapasitas produksi mencapai 3 kuintal bahan mentah seperti singkong, talas, pisang, nangka setiap minggunya, menghasilkan sekitar 5 plastik besar keripik dengan berat masing-masing 12 kg.",
-    // location: "Kampung Kopi Katindan",
     category: "kuliner",
-    // facilities: ["Area duduk indoor", "Teras outdoor", "WiFi", "Parkir"],
   },
   {
     slug: "pia-naga",
@@ -46,14 +45,7 @@ export const UMKMData: UMKMData[] = [
       "Pia lembut dengan isian lezat, Pia Naga adalah kue tradisional khas Ketindan yang cocok sebagai buah tangan.",
     detailDescription:
       "Pia Naga adalah salah satu produk kuliner khas Desa Ketindan yang populer sebagai camilan dan oleh-oleh. Seperti pia pada umumnya, Pia Naga berbentuk bulat pipih dengan kulit yang renyah di luar namun lembut di dalam, serta diisi dengan berbagai varian rasa manis. Dibuat dengan menggunakan bahan-bahan pilihan berkualitas dan melalui proses pengolahan yang menjaga tradisi, Pia Naga menawarkan perpaduan cita rasa klasik dan sentuhan khas lokal. Produk ini mencerminkan keahlian dan dedikasi pelaku UMKM di Desa Ketindan dalam menghasilkan penganan yang nikmat dan berkualitas, menjadikannya pilihan sempurna untuk dinikmati bersama keluarga atau sebagai buah tangan yang berkesan.",
-    // location: "Pia Naga",
     category: "kuliner",
-    // facilities: [
-    //   "Tour guide",
-    //   "Area petik kopi",
-    //   "Tempat pengolahan",
-    //   "Gazebo",
-    // ],
   },
   {
     slug: "batik-tulis-ketindan",
@@ -66,5 +58,15 @@ export const UMKMData: UMKMData[] = [
     // location: "Kampung Batik Ketindan",
     category: "budaya",
     // facilities: ["Workshop batik", "Galeri", "Toko souvenir", "Area parkir"],
+  },
+  {
+    slug: "w-dang-waras",
+    title: "W-dang Waras (Minuman Herbal Instan)",
+    image: wdang,
+    description:
+      "Minuman herbal instan tradisional dengan bahan alami Temulawak dan Jahe, diproduksi oleh Kelompok Binar Ketindan.",
+    detailDescription:
+      "W-dang Waras adalah produk minuman herbal instan yang menawarkan kehangatan dan manfaat kesehatan alami dari rempah-rempah pilihan, yaitu Temulawak dan Jahe. Dikemas praktis dalam bentuk bubuk di dalam botol, W-dang Waras diproduksi secara higienis oleh 'Kelompok Binar' Ketindan, sebuah UMKM lokal yang berkomitmen menghadirkan produk berkualitas dari Desa Ketindan. Temulawak dikenal kaya akan manfaat untuk kesehatan pencernaan dan daya tahan tubuh, sementara Jahe memberikan kehangatan serta membantu meredakan mual dan pegal-pegal. W-dang Waras hadir sebagai solusi minuman sehat dan praktis yang siap diseduh kapan saja, mencerminkan kekayaan rempah alami yang dimiliki desa.",
+    category: "kuliner & herbal",
   },
 ];
